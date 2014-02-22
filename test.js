e5eb06a370e597afd8fd78084507ca9fb25e32c6
Machyne <mcotter222@gmail.com>
2014-02-22 11:11:11 -0600

    Drag resize complete. New project idea though :P

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-    var resizy = (function(){
+    var isDragging;
+    $("#bar").mousedown(function() {
+        isDragging = true;
+        console.log("pressed");
+    });
 
-    })();
+    $(document).mousemove(function(e) {
+        if(isDragging) {
+            var newHeight = Math.max(Math.min(e.pageY / $('body').height() *100, 96), 4);
+            console.log("h: "+newHeight);
+            $("#top").css("height", newHeight + "%");
+            $("#bottom").css("height", (100 - newHeight) + "%");
+        }
+    });
     
+    $(document).mouseup(function() {
+        isDragging = false;
+        console.log("pressed");
+    });
 });
